88270145f35df679bfa4f938587b1d76b7569685
tammy-baylis-swi <tammy.baylis@solarwinds.com>
2025-07-18 12:19:49 -0700

    Switch to auto-instrument Django; add /reentry route

--- otel-site/polls/views.py
+++ otel-site/polls/views.py
@@ -1,5 +1,19 @@
+import logging
+
 from django.http import HttpResponse
-from django.shortcuts import render
+from opentelemetry import trace
+import requests
+
+
+logger = logging.getLogger(__name__)
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def reentry(request):
+    """A silly little view that requests the same app by http"""
+    requests.get("http://0.0.0.0:8000/polls/")
+    logger.info("Made a request to polls index.")
+    return HttpResponse("Done")
